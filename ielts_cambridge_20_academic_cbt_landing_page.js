5b08fab45f892cb60c616bc8708057e14d817161
DMIedu <niroshan.dmi@gmail.com>
2025-09-30 18:18:11 +0530

    Added initial files

---
+++ ielts_cambridge_20_academic_cbt_landing_page.js
@@ -0,0 +1,12 @@
+function LandingPage() {
+  return (
+    <div>
+      <h1>IELTS Cambridge 20 - Academic CBT</h1>
+      <p>Welcome to the landing page!</p>
+    </div>
+  );
+}
+
+// Render into the root div
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<LandingPage />);
